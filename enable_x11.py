6716d7d4a5c9270b33f3f3af3dbcf99c77b50833
jmmnn <jmmnn@yahoo.com>
2014-04-20 16:11:25 +0200

    v1

--- enable_x11.py
+++ enable_x11.py
@@ -1,40 +1,18 @@
 #!/usr/bin/env python
 __author__ = 'jmmnn'
 
-import subprocess
-import sys
-
-# python actions
-
 # Read the file
-filedata = None
-with file = open('/etc/inittab', 'r') :
+filedata = 'a'
+with open('/etc/inittab', 'r') as file:
   filedata = file.read()
+  print 'original content'
+  print filedata + '\n'
 
 # Replace the target string
-filedata.replace('id:3:initdefault', 'id:5:initdefault')
-
+filedata2 = filedata.replace('id:3:initdefault', 'id:5:initdefault')
+print 'new content'
+print filedata2 + '\n'
 # Write the file out again
-with file = open('/etc/inittab', 'w') :
-  file.write(filedata)
-
-
-##############
-#List Linux commands to execute here.
-
-#firewall and clock
-NTPD = "sudo chkconfig ntpd on"
-IPTABLES_OFF = "sudo chkconfig iptables off"
-IPTABLES_STOP = "sudo /etc/init.d/iptables stop"
-NTPD_START = "sudo service ntpd start"
-
-#order commands in sequence
-cmds = [NTPD, IPTABLES_OFF, IPTABLES_STOP, NTPD_START]
-
-#Iterates over list, running statements for each item in the list
-count=0
-for cmd in cmds:
-    count+=1
-    print "Running Command Number %s" % count
-    subprocess.call(cmd, shell=True)
+with open('/etc/inittab', 'w') as file2:
+  file2.write(filedata2)
 
