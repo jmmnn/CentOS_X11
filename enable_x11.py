79fbc86c25b3558bcb4f490cf17306267b44659d
jmmnn <jmmnn@yahoo.com>
2014-04-20 21:35:06 +0200

    v1

--- enable_x11.py
+++ enable_x11.py
@@ -1,5 +1,9 @@
 #!/usr/bin/env python
 __author__ = 'jmmnn'
+# this script sets up the RHEL or CentOS server for X11 and Gui access
+
+import subprocess
+import sys
 
 # Read the file
 filedata = 'a'
@@ -16,3 +20,24 @@ print filedata2 + '\n'
 with open('/etc/inittab', 'w') as file2:
   file2.write(filedata2)
 
+
+
+#List bash commands to execute here.
+
+# desktop, firewall, clock, reboot
+XWINDOWS = "yum groupinstall 'X Window System' Desktop"
+NTPD = "sudo chkconfig ntpd on"
+IPTABLES_OFF = "sudo chkconfig iptables off"
+IPTABLES_STOP = "sudo /etc/init.d/iptables stop"
+NTPD_START = "sudo service ntpd start"
+REBOOT = "sudo chkconfig iptables off"
+
+#order commands in sequence
+cmds = [XWINDOWS, NTPD, IPTABLES_OFF, IPTABLES_STOP, NTPD_START, REBOOT]
+
+#Iterates over list, running statements for each item in the list
+count=0
+for cmd in cmds:
+    count+=1
+    print "Running Command Number %s" % count
+    subprocess.call(cmd, shell=True)
